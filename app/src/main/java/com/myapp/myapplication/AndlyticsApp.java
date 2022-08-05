a9f7f6322fea2c08d019cc167aa5f913380058e6
azuredragon3000 <phanhuycuong2909@gmail.com>
2022-08-05 11:39:10 +0700

    1st commit

---
+++ app/src/main/java/com/myapp/myapplication/AndlyticsApp.java
@@ -0,0 +1,82 @@
+package com.myapp.myapplication;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.util.Log;
+
+import com.myapp.myapplication.db.AndlyticsDb;
+
+/*
+import org.acra.ACRA;
+import org.acra.ACRAConfiguration;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+*/
+
+/*@ReportsCrashes(sharedPreferencesMode = Context.MODE_PRIVATE, sharedPreferencesName = Preferences.PREF, mode = ReportingInteractionMode.TOAST,
+resToastText = R.string.crash_toast, sendReportsInDevMode = false)*/
+public class AndlyticsApp extends Application {
+
+	private static final String TAG = AndlyticsApp.class.getSimpleName();
+
+	private ContentAdapter db;
+
+	private static AndlyticsApp sInstance;
+
+	private boolean isAppVisible = false;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		initAcra();
+
+		// open DB here to  force schema and preferences migration
+		// the DB is kept open through the lifecycle ot the app, no need to 
+		// close here
+		AndlyticsDb.getInstance(getApplicationContext()).getWritableDatabase();
+
+		setDbAdapter(ContentAdapter.getInstance(this));
+		sInstance = this;
+	}
+
+	private void initAcra() {
+		/*try {
+			ACRA.init(this);
+			String bugsenseUrl = getResources().getString(R.string.bugsense_url);
+			HttpSender bugSenseSender = new HttpSender(HttpSender.Method.POST,
+					HttpSender.Type.FORM, bugsenseUrl, null);
+			ACRA.getErrorReporter().setReportSender(bugSenseSender);
+		} catch (IllegalStateException e) {
+			Log.w(TAG, "ACRA.init() called more than once?: " + e.getMessage(), e);
+		}*/
+	}
+
+	public boolean isDebug() {
+		return (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) > 0;
+	}
+
+	public static AndlyticsApp getInstance() {
+		return sInstance;
+	}
+
+	public boolean isAppVisible() {
+		// TODO This is a bit of a hack, could it be improved
+		return isAppVisible;
+	}
+
+	public void setIsAppVisible(boolean isVisible) {
+		isAppVisible = isVisible;
+	}
+
+	public void setDbAdapter(ContentAdapter db) {
+		this.db = db;
+	}
+
+	public ContentAdapter getDbAdapter() {
+		return db;
+	}
+
+}
---
+++ app/src/main/java/com/myapp/myapplication/sync/AutosyncHandler.java
@@ -0,0 +1,69 @@
+
+package com.myapp.myapplication.sync;
+
+import java.util.List;
+
+import android.accounts.Account;
+import android.content.ContentResolver;
+import android.content.PeriodicSync;
+import android.os.Bundle;
+
+
+public class AutosyncHandler {	
+
+	public static int DEFAULT_PERIOD = 60 * 3; // 3 hours (in minutes)
+	public static final String ACCOUNT_AUTHORITY = "com.github.andlyticsproject";
+	public static final String ACCOUNT_TYPE_GOOGLE = "com.google";
+
+	public boolean isAutosyncEnabled(String accountName) {
+		Account account = new Account(accountName, AutosyncHandler.ACCOUNT_TYPE_GOOGLE);
+		return ContentResolver.getSyncAutomatically(account, AutosyncHandler.ACCOUNT_AUTHORITY);
+	}
+	
+	public void setAutosyncEnabled(String accountName, boolean enabled) {
+		Account account = new Account(accountName, AutosyncHandler.ACCOUNT_TYPE_GOOGLE);
+		ContentResolver.setSyncAutomatically(account, AutosyncHandler.ACCOUNT_AUTHORITY, enabled);
+	}
+
+	/**
+	 * Gets the sync period in minutes for the given account
+	 * @param accountName
+	 * @return The sync period in minutes
+	 */
+	public int getAutosyncPeriod(String accountName) {
+
+		int result = 0;
+
+		Account account = new Account(accountName, AutosyncHandler.ACCOUNT_TYPE_GOOGLE);
+		if (ContentResolver.getSyncAutomatically(account, AutosyncHandler.ACCOUNT_AUTHORITY)) {
+			List<PeriodicSync> periodicSyncs = ContentResolver.getPeriodicSyncs(account,
+					AutosyncHandler.ACCOUNT_AUTHORITY);
+			for (PeriodicSync periodicSync : periodicSyncs) {
+				result = 60 * (int) periodicSync.period;
+				break;
+			}
+		}
+		return result;
+
+	}
+
+	/**
+	 * Sets the sync period in minutes for the given account
+	 * @param accountName
+	 * @param periodInMins
+	 */
+	public void setAutosyncPeriod(String accountName, Integer periodInMins) {
+
+		Bundle extras = new Bundle();
+		Account account = new Account(accountName, AutosyncHandler.ACCOUNT_TYPE_GOOGLE);
+
+		if (periodInMins == 0) {
+			ContentResolver.setSyncAutomatically(account, AutosyncHandler.ACCOUNT_AUTHORITY, false);
+		} else {
+			ContentResolver.setSyncAutomatically(account, AutosyncHandler.ACCOUNT_AUTHORITY, true);
+			ContentResolver.addPeriodicSync(account, AutosyncHandler.ACCOUNT_AUTHORITY, extras, periodInMins * 60);
+		}
+		
+	}
+
+}
